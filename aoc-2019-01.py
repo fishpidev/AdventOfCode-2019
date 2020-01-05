4165692a57450cdb5372815c0e404008338b2a31
FishPiDev <fishpidev@gmail.com>
2020-01-05 16:45:58 +0000

    more simple file name constructin and data to list

--- aoc-2019-01.py
+++ aoc-2019-01.py
@@ -3,13 +3,17 @@ Advent of Code Day 1
 Puzzle summary: "...to find the fuel required for a module, take its mass,
 divide by three, round down, and subtract 2..."
 '''
-from pathlib import Path 
 
 #read the input file
-THIS_FILE = __file__
-inputDataFName = Path(THIS_FILE[:-3] + "-input.txt")
+#THIS_FILE = __file__
 
-inputData = Path.read_text(inputDataFName)
-print(inputData)
+def read_input_data(script_name=__file__, data_name_ext="-input.txt"):
 
+    fileNameIn = script_name[:-3] + data_name_ext
+    with open(fileNameIn, "r") as f:
+        content = f.readlines()
+        
+    content = [x.strip() for x in content]#straight from SO, remove whitespace
+    return(content)
 
+data = read_input_data()
