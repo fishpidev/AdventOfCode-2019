fc9a451b1567bfc9e939498d84f9ae68f9caa283
fishpidev <fishpidev@gmail.com>
2020-02-16 19:40:44 +0000

    passing tests for part 2

--- test/test_utils_aoc_2019.py
+++ test/test_utils_aoc_2019.py
@@ -2,7 +2,7 @@
 Test for Advent of code 2019 common utility functions
 
 '''
-from aoc_2019_01 import calcualte_fuel_simple
+from aoc_2019_01 import calcualte_fuel_simple, compute_fuel_required_complex
 import pytest
 
 @pytest.mark.parametrize("in_mass, out_fuel", [
@@ -13,3 +13,12 @@ import pytest
         ])
 def test_simple_fuel_calc(in_mass, out_fuel):
     assert calcualte_fuel_simple(in_mass) == out_fuel
+    
+    
+@pytest.mark.parametrize("in_mass_complex, out_fuel_complex", [
+    ([14], [2]),
+    ([1969], [966]),
+    ([100756], [50346])
+    ])
+def test_complex_fule_calculation(in_mass_complex, out_fuel_complex):
+    assert compute_fuel_required_complex(in_mass_complex) == out_fuel_complex
