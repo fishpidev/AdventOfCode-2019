4c01e691c5b88c58166687192513cb401174dbfc
fishpidev <fishpidev@gmail.com>
2020-02-22 18:14:27 +0000

    doc strings and changes having ran pylint

--- aoc_2019_parallel_01.py
+++ aoc_2019_parallel_01.py
@@ -9,11 +9,38 @@ import utils_aoc_2019 as utils
 
 
 def calculate_fuel_simple(mass: int) -> int:
+    """
+    The basic function for simple fuel calacualte 
+    
+    Parameters
+    ----------
+    mass : int
+    the mass to calcualte fuel for
+    
+    Returns
+    -------
+    int
+    the fuel needed 
+    """
     return math.floor(mass / 3)-2
 
 def calculate_fuel_complex(mass: int) -> Iterable[int]:
-    fuelFuel = []
+    """
+    The complex function for fuel calacualte.
+    Implements the "part 2" rules, calculating fuel "cost" for the mass of
+    the additional fuel
     
+    Parameters
+    ----------
+    mass : int
+    the mass to calcualte fuel for
+    
+    Returns
+    -------
+    List
+    the fuel needed 
+    """
+    fuelFuel = []    
     while mass > 0:
         inter_fuel = math.floor(mass / 3)-2
         if inter_fuel > 0:
@@ -21,31 +48,53 @@ def calculate_fuel_complex(mass: int) -> Iterable[int]:
         mass = inter_fuel
     return sum(fuelFuel)
 
-def compute_fuel_required(massList: Iterable[int]) -> Iterable[int]:
+def compute_fuel_required(mass_list: Iterable[int]) -> int:
+    """
+    A Parallel simple fuel calcualtion with the Pool object.
     
+    Parameters
+    ----------
+    mass_list : Iterable 
+    the list of mass values to calcualte fuel for
+    
+    Returns
+    -------
+    int
+    the fuel needed 
+    """
     pool = Pool()
-    results = pool.map(calculate_fuel_simple, massList)
+    results = pool.map(calculate_fuel_simple, mass_list)
     pool.close()
     pool.join()
-    
     return sum(results)
 
-def compute_fuel_required_complex(massList :Iterable[int]) -> Iterable[int]:
+def compute_fuel_required_complex(mass_list: Iterable[int]) -> int:
+    """
+    A Parallel complex fuel calcualtion with the Pool object.
     
+    Parameters
+    ----------
+    mass_list : Iterable 
+    the list of mass values to calcualte fuel for
+    
+    Returns
+    -------
+    int
+    the fuel needed 
+    """
     pool = Pool()
-    results = pool.map(calculate_fuel_complex, massList)
+    results = pool.map(calculate_fuel_complex, mass_list)
     pool.close()
-    pool.join()
-        
+    pool.join()        
     return sum(results)
 
-data = utils.read_input_data(script_name=__file__)
+DATA = utils.read_input_data(script_name=__file__)
 
 #Answer to part 1
-print(compute_fuel_required(massList=data))
+print(compute_fuel_required(mass_list=DATA))
 
 #Answer to part 2
-partTwo = compute_fuel_required_complex(massList=data)
+partTwo = compute_fuel_required_complex(mass_list=DATA)
 
 try:
     utils.in_lights(partTwo)
