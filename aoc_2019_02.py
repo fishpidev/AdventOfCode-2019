5b0c8879aa1bde8da1576e677de509df1a4fed04
fishpidev <fishpidev@gmail.com>
2020-03-22 18:36:14 +0000

    new tests for Day 2, making Day 2 code functional

--- aoc_2019_02.py
+++ aoc_2019_02.py
@@ -2,16 +2,11 @@
 Advent of Code Day 2
 '''
 from typing import Iterable
-import sys
 
-#from multiprocessing import Pool
 import utils_aoc_2019 as utils
 
 intcode_program = utils.read_input_incode(script_name=__file__, use_test_data=True)
-print(intcode_program)
-print("\n")
-
-#sys.exit()
+print(f"Input Incode programme: {intcode_program}")
 
 def operate(operate_flag: int, pos_one: int, pos_two: int):
     if operate_flag == 1:
@@ -19,20 +14,25 @@ def operate(operate_flag: int, pos_one: int, pos_two: int):
     if operate_flag == 2:
         return pos_one * pos_two
 
-for i, val in enumerate(intcode_program):
-        
-    if val == 99:
-        print(intcode_program)
-        break
-    elif i % 4 == 0:
-        out_index = intcode_program[i+3]
-        
-        index_of_input_one = i+1
-        index_of_input_two = i+2
-        
-        input_value_idx_one = intcode_program[index_of_input_one]
-        input_value_idx_two = intcode_program[index_of_input_two]
-        
-        result = operate(val, intcode_program[input_value_idx_one], intcode_program[input_value_idx_two])
-        intcode_program[out_index] = result
-        print(intcode_program)#initial test works - now add the other tests etc 
+def execute_intcode_programme(input_intcode_programme: Iterable[int]) -> Iterable[int]:
+    
+    for i, val in enumerate(input_intcode_programme):
+            
+        if val == 99:
+            break
+        elif i % 4 == 0:
+            out_index = input_intcode_programme[i+3]
+            
+            index_of_input_one = i+1
+            index_of_input_two = i+2
+            
+            input_value_idx_one = input_intcode_programme[index_of_input_one]
+            input_value_idx_two = input_intcode_programme[index_of_input_two]
+            
+            result = operate(val, input_intcode_programme[input_value_idx_one], input_intcode_programme[input_value_idx_two])
+            input_intcode_programme[out_index] = result
+    
+    return input_intcode_programme
+
+part_two_a = execute_intcode_programme(intcode_program)
+print(part_two_a)
--- test/test_utils_aoc_2019.py
+++ test/test_utils_aoc_2019.py
@@ -2,9 +2,12 @@
 Test for Advent of code 2019 common utility functions
 
 '''
-from aoc_2019_01 import calcualte_fuel_simple, compute_fuel_required_complex
+from aoc_2019_01 import calculate_fuel_simple, compute_fuel_required_complex
+from aoc_2019_02 import execute_intcode_programme
+
 import pytest
 
+#aoc Day 1
 @pytest.mark.parametrize("in_mass, out_fuel", [
         (12, 2),
         (14, 2),
@@ -12,7 +15,7 @@ import pytest
         (100756, 33583)
         ])
 def test_simple_fuel_calc(in_mass, out_fuel):
-    assert calcualte_fuel_simple(in_mass) == out_fuel
+    assert calculate_fuel_simple(in_mass) == out_fuel
     
     
 @pytest.mark.parametrize("in_mass_complex, out_fuel_complex", [
@@ -22,3 +25,13 @@ def test_simple_fuel_calc(in_mass, out_fuel):
     ])
 def test_complex_fule_calculation(in_mass_complex, out_fuel_complex):
     assert compute_fuel_required_complex(in_mass_complex) == out_fuel_complex
+    
+#aoc Day 2
+@pytest.mark.parametrize("in_intcode, output_intcode", [
+        ([1,0,0,0,99], [2,0,0,0,99]),
+         ([2,3,0,3,99], [2,3,0,6,99]),
+         ([2,4,4,5,99,0], [2,4,4,5,99,9801]),
+         ([1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99])
+         ])
+def test_execute_intcode_programme(in_intcode, output_intcode):
+    assert execute_intcode_programme(in_intcode) == output_intcode
